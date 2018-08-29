d528ab5e9c414dd1b4a0877c35a31f879a9eb1f4
Chris Tian <chris.shang.tian@gmail.com>
2018-08-29 16:17:29 -0500

    Moved from Desktop

---
+++ CalenderApp/calenderproject/calenderproject/urls.py
@@ -0,0 +1,31 @@
+"""calenderproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('calender', views.calender, name='calender'),
+    path('login', auth_views.login, name='login'),
+    path('logout', auth_views.logout, name='logout'),
+    path('entry/<int:pk>', views.details, name='details'),
+    path('entry/add', views.add, name='add'),
+    path('entry/delete/<int:pk>', views.delete, name='delete'),
+
+    path('admin/', admin.site.urls),
+]
---
+++ CalenderApp/calenderproject/myapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import Entry
+from .forms import EntryForm
+
+# Create your views here.
+def index(request):
+    return render(request, 'myapp/index.html')
+
+def calender(request):
+    entries = Entry.objects.all()
+    return render(request, 'myapp/calender.html', {'entries': entries})
+
+def details(request, pk):
+    entry = Entry.objects.get(id=pk)
+    return render(request, 'myapp/details.html', {'entry': entry})
+
+def add(request):
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            date = form.cleaned_data['date']
+            description = form.cleaned_data['description']
+
+            Entry.objects.create(
+                name=name,
+                date=date,
+                description=description,
+            ).save()
+
+            return HttpResponseRedirect('/')
+    else:
+        form = EntryForm()
+
+    return render(request, 'myapp/form.html', {'form': form})
+
+def delete(request, pk):
+    if request.method == "DELETE":
+        entry = get_object_or_404(Entry, pk=pk)
+        entry.delete()
+
+    return HttpResponseRedirect('/')
